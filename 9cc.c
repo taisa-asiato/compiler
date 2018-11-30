8638b97f21d7fe27f69212813486fb26ff6ba305
taisa-asiato <taisa.aikou@gmail.com>
2018-11-30 12:52:42 +0900

    足し算と引き算を追加

--- 9cc.c
+++ 9cc.c
@@ -8,10 +8,31 @@ int main( int argc, char **argv )
 		return 1;
 	}
 
+	char * p = argv[1];
+
 	printf( ".intel_syntax noprefix\n" );
 	printf( ".global main\n" );
 	printf( "main:\n" );
-	printf( "	MOV	rax, %d\n", atoi( argv[1] ) );
+	printf( "	MOV	rax, %ld\n", strtol( p, &p, 10 ) );
+
+	while ( *p ) {
+		if ( *p == '+' ) {
+			p++;
+			printf( "	ADD 	rax, %ld\n", strtol( p, &p, 10 ) );
+			continue;
+		}
+
+		if ( *p == '-' ) {
+			p++;
+			printf( "	SUB	rax, %ld\n", strtol( p, &p, 10 ) );
+			continue;
+		}
+
+		fprintf( stdout, "予期せぬ文字です: '%c'\n", *p );
+		return 1;
+	}
+
+
 	printf( "	RET\n");
 	return 0;
 }
