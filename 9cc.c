80aba5e038b8140d76a476f3a1237699b4672dcb
taisa-asiato <taisa.aikou@gmail.com>
2018-12-13 23:46:16 +0900

    トークナイザ, 生成規則に基づいて構文木作成関数, スタックマシン用関数の実相 メイン関数を変更していないためバグると思う.

--- 9cc.c
+++ 9cc.c
@@ -9,6 +9,11 @@ enum {
 	TK_EOF,		// 入力の終わりを表すトークン
 };
 
+
+enum {
+	ND_NUM = 256,	// 整数ノードを示す
+};
+
 // トークンの型
 typedef struct {
 	int ty;		//トークンの型
@@ -16,9 +21,58 @@ typedef struct {
 	char * input;	// トークン文字列（エラーメッセージ用)
 } Token;
 
+typedef struct Node {
+	int ty;
+	struct Node * lhs;
+	struct Node * rhs;
+	int val;
+} Node;
+
 //トークナイズした結果のトークン列はこの配列に保存する
 // 100 以上のトークンは来ないものとする
 Token tokens[100];
+int pos = 0;
+
+void tokenize( char * p );
+Node * new_node( int ty, Node *lhs, Node * rhs );
+Node * new_node_num( int val );
+Node * expr();
+Node * mul();
+Node * term();
+void error( int i );
+void gen( Node * node );
+
+// スタックマシン
+void gen( Node * node )
+{
+	if ( node->ty = ND_NUM ) {
+		printf( "	push	%d\n", node->val );
+		return;
+	}
+
+	gen( node->lhs );
+	gen( node->rhs );
+
+	printf( "	pop	rdi\n" );
+	printf( "	pop 	rax\n" );
+
+	switch ( node->ty ) {
+		case '+':
+			printf( "	add	rax, rdi\n" );
+			break;
+		case '-':
+			printf( "	sub	rax, rdi\n" );
+			break;
+		case '*':
+			printf( "	mul	rdi\n" );
+			break:
+		case '/':
+			printf( "	mov	rdx, 0\n" );
+			printf( " 	div	rdx\n" );
+			break;
+	}
+	printf( "	push	 rax\n" ); 
+}
 
 /* pがさしている文字列をトークンに分割する */
 void tokenize( char * p ) 
@@ -58,6 +112,78 @@ void tokenize( char * p )
 	tokens[i].input = p;
 }
 
+// 新しくNodeを追加する
+// 追加されるNodeは,演算子
+Node * new_node( int ty, Node *lhs, Node * rhs )
+{
+	Node * node = malloc( sizeof( Node ) );
+	node->ty = ty;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+
+// 新しくNode(数値)追加する
+Node * new_node_num( int val )
+{
+	Node * node = malloc( sizeof( Node ) );
+	node->ty = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+// 生成規則, expr
+Node * expr()
+{
+	Node * lhs = mul();
+	if ( tokens[pos].ty == '+' ) {
+		pos++;
+		return new_node( '+', lhs, expr() );
+	}
+	if ( tokens[pos].ty == '-' ) {
+		pos++;
+		return new_node( '-', lhs, expr() );
+	}
+
+	return lhs;
+}
+
+// 生成規則, mul
+Node * mul()
+{
+	Node * lhs = term();
+	if ( tokens[pos].ty == '*' ) {
+		pos++;
+		return new_node( '*', lhs, mul() );
+	}
+	if ( tokens[pos].ty == '/' ) {
+		pos++;
+		return new_node( '/', lhs, mul() );
+	}
+
+	return lhs;
+}
+
+
+// 生成規則, term
+Node * term()
+{
+	if ( tokens[pos].ty == TK_NUM ) {
+		return new_node_num( tokens[pos++].val );
+	}
+	if ( tokens[pos].ty == '(' ) {
+		pos++;
+		Node * node =  expr();
+		if ( tokens[pos].ty != ')' ){
+			error( "開き括弧に対する綴じ括弧がありません: %s\n", tokens[pos].input );
+		}
+		pos++;
+		return node;
+
+	}
+	error( "数値でも開き括弧でもないトークンです: %s\n", tokens[pos].input );
+}
 
 // エラー報告用の関数
 void error( int i )
@@ -85,14 +211,14 @@ int main( int argc, char **argv )
 
 	// 式の最初は数値のでなければならないので,それをチェック
 	if ( tokens[0].ty != TK_NUM ) { error( 0 ); }
-	printf( "	MOV	rax, %d\n", tokens[0].val );
+	printf( "	mov	rax, %d\n", tokens[0].val );
 
 	int i = 1;
 	while ( tokens[i].ty != TK_EOF ) {
 		if ( tokens[i].ty == '+' ) {
 			i++;
 			if ( tokens[i].ty != TK_NUM ) { error( i ); }
-			printf( "	ADD 	rax, %d\n", tokens[i].val );
+			printf( "	add 	rax, %d\n", tokens[i].val );
 			i++;
 			continue;
 		}
@@ -100,7 +226,7 @@ int main( int argc, char **argv )
 		if ( tokens[i].ty == '-' ) {
 			i++;
 			if ( tokens[i].ty != TK_NUM ) { error( i ); }
-			printf( "	SUB	rax, %d\n", tokens[i].val );
+			printf( "	sub	rax, %d\n", tokens[i].val );
 			i++;
 			continue;
 		}
@@ -108,7 +234,7 @@ int main( int argc, char **argv )
 	}
 
 
-	printf( "	RET\n");
+	printf( "	ret\n");
 	return 0;
 }
 
